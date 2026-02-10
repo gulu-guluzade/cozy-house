415058815edfac091c87f87acc473ce909479aaa
gulu-guluzade <mr.guluzade.official@gmail.com>
2026-02-11 03:25:50 +0400

    Section about added to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import About from '@/components/about';
 import Hero from '@/components/hero';
 
 const Home = () => {
   return (
     <main>
       <Hero />
+      <About />
     </main>
   );
 };
