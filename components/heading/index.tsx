48ff2cb8ae7d89069b570e0ea28f90039ee89e45
gulu-guluzade <mr.guluzade.official@gmail.com>
2026-02-11 04:24:26 +0400

    Added props classNames for tag h2 and its parent div

--- components/heading/index.tsx
+++ components/heading/index.tsx
@@ -1,19 +1,22 @@
-interface IHeading {
+interface IHeadingProps {
   heading: string;
+  className?: string;
+  parentClassName?: string;
 }
 
-const Heading = ({ heading }: IHeading) => {
+const Heading = ({ heading, className, parentClassName }: IHeadingProps) => {
   return (
-    <div>
+    <div className={parentClassName}>
       <h2
-        className='
-        text-[#545454]
-        leading-snug 
-        tracking-wide 
-        text-[25px] 
-        text-center
-        md:text-[35px] 
-        md:text-start'
+        className={`
+          text-[#545454]
+          leading-snug 
+          tracking-wide 
+          text-[25px] 
+          text-center
+          md:text-[35px] 
+          md:text-start
+          ${className}`}
       >
         {heading}
       </h2>
